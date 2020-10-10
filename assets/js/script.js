e22eb315d248611808aca2562fe82bcf8df7882e
Pedal <jmb181@humboldt.edu>
2020-10-10 16:05:01 -0700

    Initial directory setup

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var timer = 90;
+var questions = [
+    {q:"What does API stand for?", options:["Application Persistence Index", "Application Programming Interface", "American Petroleum Institute", "Array Processing Index"], a: 1},
+    {q:"A very useful tool used during development and debugging for printing content to the debugger is:", options:["JavaScript","for loops","console.log","gitbash"], a:2},
+    {q:"Commonly used data types do NOT include:", options:["strings", "booleans", "numbers", "alerts"], a:3}
+];
+
+var timerEl = document.querySelector("#timer");
+
